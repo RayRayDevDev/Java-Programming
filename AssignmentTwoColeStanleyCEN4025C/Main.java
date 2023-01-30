32fa3efa8c5fec35eff762bd8e93b58d281fb06d
RäDev <github@rayraydev.dev>
2023-01-30 13:08:24 -0700

    Got Adding and Viewing the ArrayList to work.

--- AssignmentTwoColeStanleyCEN4025C/Main.java
+++ AssignmentTwoColeStanleyCEN4025C/Main.java
@@ -7,35 +7,47 @@ class Main {
     public static void main(String[] args) {
         ArrayList<String> toDo = new ArrayList<>();
         Scanner userChoice = new Scanner(System.in); // For the Menu Selection.
+        int userInput = 0;
 
-        while (userChoice != 4) {
+        while (userInput != 4) {
             // TODO: Implement User Selection menu.
-            // 1. View todo list.
-            ViewToDoList(toDo);
-            // 2. Add to todo list.
-            // 3. Delete from todo list.
-            // 4. Exit.
+            System.out.println(
+                    "1. View your To-Do List\n2. Add to your To-Do List\n3. Delete items within your To-Do List\n4. Exit");
+            userInput = userChoice.nextInt();
+            switch (userInput) {
+                case 1:
+                    ViewToDoList(toDo);
+                    break;
+                case 2:
+                    addToToDoList(toDo);
+                    break;
+                case 3:
+                    DeleteOnToDoList(toDo);
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+            }
 
         }
     }
 
     public static void ViewToDoList(ArrayList toDoList) {
-        for (i = 0; i < toDoList.size(); i++) {
-            System.out.println(Arrays.toString(toDoList));
+        for (int i = 0; i < toDoList.size(); i++) {
+            System.out.println(toDoList.get(i));
         }
     }
 
     public static ArrayList addToToDoList(ArrayList toDoList) {
         Scanner userInput = new Scanner(System.in); // For the user's actual input.
-        while (userInput != -1) { // Less likely trigger in a console application.
-            toDoList.add(userInput);
+        String userChoice = " ";
+        while (!userChoice.equals("-1")) { // Less likely trigger in a console application.
+            userChoice = userInput.nextLine();
+            toDoList.add(userChoice);
         }
 
         return toDoList;
 
-        if (userInput == -1) {
-            break;
-        }
     }
 
     public static ArrayList DeleteOnToDoList(ArrayList toDoList) {
